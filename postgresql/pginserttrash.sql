0dfbb8159138c8e9113095f63ff6399ba9b93c51
clementlr <clement.leroux@gmail.com>
2020-04-03 17:58:18 +0200

    adding PostGre insert operation with Point() parameter

---
+++ postgresql/pginserttrash.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.trash (id, id_ref_campaign_fk,the_geom, elevation, id_ref_trash_type_fk,brand_type ) 
+VALUES (DEFAULT, 'fb367e0e-9b56-4418-9b80-5acd5c239754','POINT(448538.11363792856 6216901.666926357)','610.109375',1,'coca')
+RETURNING id;		
+id = 057cca5a-4848-4b95-b157-082be3a1a173	
+id = d6315b55-1a76-4e41-83b1-da5401e326e1
