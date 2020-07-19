04ad1f7c62cf5e3986107843850aba914f67e6cd
clementlr <clement.leroux@gmail.com>
2020-07-19 17:23:56 +0200

    blob trigger az function + cleaning

--- azfunction/etlBlobTrigger/etlBlobTriggerManual/__init__.py
+++ azfunction/etlBlobTrigger/etlBlobTriggerManual/__init__.py
@@ -10,10 +10,14 @@ def main(etlblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {etlblob.name}\n"
                  f"Blob Size: {etlblob.length} bytes")
-
-    url = f'http://etlapiplastico-dev.westeurope.cloudapp.azure.com:8082/api/etlHttpTrigger?container=campaign0&blob={etlblob.name}&prediction=json&source=gopro&target=csv'
+    blob_fullname = etlblob.name
+    container = blob_fullname.split('/')[0]
+    blob_name = blob_fullname.split('/')[1]
+    source = 'manual'
+    url = f'http://etlapiplastico-vmss-dev-ds3.westeurope.cloudapp.azure.com:8082/api/etlHttpTrigger?container={container}&blob={blob_name}&prediction=json&source={source}&target=csv'
     payload = {}
     headers= {}
     response = requests.request("GET", url, headers=headers, data = payload)
     logging.info(response.text.encode('utf8'))
+    logging.info(f'Sent Request to ETL to process blob {blob_name} from Manual source')
     
