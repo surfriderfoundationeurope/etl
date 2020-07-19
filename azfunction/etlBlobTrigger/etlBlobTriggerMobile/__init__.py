95199ca9865b9eef7395c3bd910aa7514364e0f6
clementlr <clement.leroux@gmail.com>
2020-07-19 17:23:56 +0200

    blob trigger az function + cleaning

---
+++ azfunction/etlBlobTrigger/etlBlobTriggerMobile/__init__.py
@@ -0,0 +1,23 @@
+import logging
+import requests
+
+import azure.functions as func
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main(etlblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {etlblob.name}\n"
+                 f"Blob Size: {etlblob.length} bytes")
+    blob_fullname = etlblob.name
+    container = blob_fullname.split('/')[0]
+    blob_name = blob_fullname.split('/')[1]
+    source = 'mobile'
+    url = f'http://etlapiplastico-vmss-dev-ds3.westeurope.cloudapp.azure.com:8082/api/etlHttpTrigger?container={container}&blob={blob_name}&prediction=json&source={source}&target=csv'
+    payload = {}
+    headers= {}
+    response = requests.request("GET", url, headers=headers, data = payload)
+    logging.info(response.text.encode('utf8'))
+    logging.info(f'Sent Request to ETL to process blob {blob_name} from Mobile source')
+    
