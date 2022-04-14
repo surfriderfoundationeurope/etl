88c4a85391434bfafb7ab992164994ca09124762
clementlr <clement.leroux@gmail.com>
2022-04-14 15:12:53 +0200

    cleaning notebooks

--- notebooks/curl_request.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -F "file=@/tmp/vid1-4K4.MP4" http://aiapisurfrider.northeurope.cloudapp.azure.com:5000
--- notebooks/curl_request_param.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -F "file=@/tmp/$1" http://aiapisurfrider.northeurope.cloudapp.azure.com:5000
