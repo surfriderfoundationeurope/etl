46061c034e40d9ac1eb2c2c4fa22082ae094d447
clementlr <clement.leroux@gmail.com>
2022-05-31 17:43:43 +0200

    updating azure function blob trigger for batch

--- src/batch/etlBlobTrigger/etlBlobTriggerMobile/__init__.py
+++ src/batch/etlBlobTrigger/etlBlobTriggerMobile/__init__.py
@@ -1,7 +1,9 @@
 import logging
+import os
 import requests
 
 import azure.functions as func
+from .postgre import get_log_df, get_pg_connection_string, open_pg_connection, insert_log_etl_df
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,15 +12,28 @@ def main(etlblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {etlblob.name}\n"
                  f"Blob Size: {etlblob.length} bytes")
+    # Blob info
     blob_fullname = etlblob.name
     container = blob_fullname.split('/')[0]
     blob_name = blob_fullname.split('/')[1]
-    source = 'mobile'
-    aiurl = '<aiurl>'
-    url = f'<url>'
-    payload = {}
-    headers= {}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    logging.info(response.text.encode('utf8'))
-    logging.info(f'Sent Request to ETL to process blob {blob_name} from Mobile source')
-    
+    # Media info
+    campaign_id = os.path.splitext(blob_name)[0]
+    media_name = blob_name
+    media_id = campaign_id
+    # Log Dataframe
+    etl_log_df = get_log_df(campaign_id,media_id,media_name)
+    # PG connection
+    pg_conn_string = get_pg_connection_string()
+    pg_connection = open_pg_connection(pg_conn_string)
+    pg_cursor = pg_connection.cursor()
+    # Log insert
+    for i, row in etl_log_df.iterrows():
+        try:
+            log_id = insert_log_etl_df(row,pg_cursor,pg_connection,'mobile')
+            logging.info(f'Successfully insterted new log: {log_id}')
+        except:
+            log_id = row['campaign_id']
+            logging.error(f'There was an issue inserting log id: {log_id} within PostGre')
+            logging.error("Early exit of ETL workflow as PG INSERT failed")
+            exit()    
+   
